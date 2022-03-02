b155dfcbc72bd3fa6a8f3b9cba4631f9ca993f5d
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 16:04:03 +0600

    Create else.py

---
+++ example-2/else/else.py
@@ -0,0 +1,11 @@
+# This example demonstrates use of if else statment
+
+age = int(input(“Your age:”))
+
+if age > 20:
+  
+     print(“Eligible to vote”)
+    
+else:
+  
+     print(“Not eligible to vote”)
