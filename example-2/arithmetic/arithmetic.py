973a234f58a3f6a4f0e989ea38c9da6c3a5d3266
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 15:26:42 +0600

    Create arithmetic.py

---
+++ example-2/arithmetic/arithmetic.py
@@ -0,0 +1,10 @@
+# This example show use of modulo(%), a arithmetic operator
+
+# Modulo(%) is used to get the remainder part of a division
+
+num1 = 5 % 3
+
+print(num1)
+
+# output 2, when we divide 5 / 3
+# the reminder is 2
