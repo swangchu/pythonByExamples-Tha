281f1a345dbe43843acc2a5133756ecceb1d3921
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 12:29:55 +0600

    Create dataType.py

---
+++ example-1/DataTypes/dataType.py
@@ -0,0 +1,24 @@
+# This example prints out different data types
+# ----------------------
+
+# totalSubject is variable stores a value 4 
+# so the data type of totalSubject is an integer
+totalSubject = 4  
+
+# Since variable averageMark stores 78.5 so the data type is a float
+averageMark = 78.5 
+
+# The variable studentName stores "Gaki Pelzom" which is a string data type
+studentName= "Gaki Pelzom" 
+
+# The variable qualified stores value true which is boolean data type
+qualified = true  
+
+# When we use variable it should be in double quote
+print(totalSubject)
+
+print(averageMark)
+
+print(studentName)
+
+print(“Qualified ”,qualified)
