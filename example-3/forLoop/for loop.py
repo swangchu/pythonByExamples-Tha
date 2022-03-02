dee296131aae1f4b6ca99249ca95d8f2428e52dc
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 19:11:06 +0600

    Create for loop.py

---
+++ example-3/forLoop/for loop.py
@@ -0,0 +1,42 @@
+# This example demonstrates use of for loops 
+
+# This for loop prints “Om Mani payme hung!” five times.
+# i will iterate from 0 till 5. 
+
+for i in range(5):
+  
+     print(“Om Mani payme hung!”)
+ 
+
+
+
+# This for loop prints number from 0 to 4
+
+for i in range(5):
+  
+     print(i)
+    
+# Output : 0 1 2 3 4
+
+
+
+# This for loop from 2 to 9
+# 2 is the starting number and 10 is the end number but excluded. 
+# By default the value increases by 1
+
+for j in range(2,10):
+  
+     print(j)
+    
+# Output: 2 3 4 5 6 7 8 9
+
+
+
+# This for loop starts from 1 and end less than 10.
+# 3 represents steps with which the number increases.
+
+for k in range(1,10,3):
+  
+     print(k)
+    
+# Output: 1 4 7
