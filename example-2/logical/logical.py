54077715b7821e2b39c81548f1c5303bdd6bfa5d
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 16:14:15 +0600

    Create logical.py

---
+++ example-2/logical/logical.py
@@ -0,0 +1,17 @@
+# This example demonstrate and operator
+
+age = int(input(“Your age:”))
+
+# 'and' is a logical operator
+if age > 18 and age < 60:
+  
+   print(“You must be earning”)
+    
+elif age < 18 and age > 6:
+  
+   print(“You must be studying”)
+    
+else:
+  
+  print(“Happy retirement”)
+  
