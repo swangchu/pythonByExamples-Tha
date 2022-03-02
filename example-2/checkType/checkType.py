3f5d3d0644cc91b0891345b72e94a9d693dd3044
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 15:01:09 +0600

    Create checkType.py

---
+++ example-2/checkType/checkType.py
@@ -0,0 +1,25 @@
+# This example is to check the data types of a variable
+# -----------------------------------------------------
+
+name = "Sonam"
+
+age = 16
+
+height = 167.8
+
+passed = True
+
+# type() function is used to check the data type of a variable
+
+print(type(name)) 
+# Output <class 'str'> meaning variable, name, stores a string value
+
+print(type(age)) 
+# Output <class 'int'>  meaning variable, age, stores a integer value
+
+print(type(height))#
+# Output <class ’float’>   meaning variable, height, stores a float value
+
+print(type(passed))
+#Ouput <class ‘bool'> meaning variable, passed, stores a boolean value
+
