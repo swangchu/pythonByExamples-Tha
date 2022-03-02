80fd4772252be69d486d6975742b295322a4cd03
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 15:20:29 +0600

    Create comment.py

---
+++ example-2/comments/comment.py
@@ -0,0 +1,11 @@
+# This example shows the use of comments
+
+"""
+This is multiline comments. These
+comments are enclosed in triple
+quotes
+"""
+
+eng = 78 # Marks obtained in english
+
+# This is single line comments
