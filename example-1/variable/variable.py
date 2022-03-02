7c51814f77a2aec318efe4de83eb285ec5842c81
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 12:03:06 +0600

    Create variable.py

---
+++ example-1/variable/variable.py
@@ -0,0 +1,22 @@
+# name is a variable and sonam is a string value 
+# = is an assignment operator
+name = “Sonam”
+
+# age is a variable and 16 is a integer value
+age = 16
+
+# contactNumber is a variable 
+contactNumber = 17767999
+
+# "My name is " is a string 
+# + is a concatenation operator that joins two strings
+# variable name is being used
+
+print(“My name is ”+name)
+
+# variable age being used
+# comma (,) is used when part is a string and other is an integer(number)
+print(“I am ”,age)
+
+# variable contactNumber is used and the program runs it is replace with 17767999
+print(“Contact ”,contactNumber)
