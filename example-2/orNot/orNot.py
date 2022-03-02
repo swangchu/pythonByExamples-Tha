e0e4f6925d5cb137f5cae70e4b8aeb0af048fd31
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 16:29:19 +0600

    Create orNot.py

---
+++ example-2/orNot/orNot.py
@@ -0,0 +1,16 @@
+#This example demonstrate use of 'or' and 'not' logical operator
+
+
+mark = 77
+
+if mark > 70 or mark == 100:
+  
+   print(“Qualified”)
+    
+elif mark not 0:
+  
+   print(“Scored some marks”)
+    
+else:
+  
+   print(“Good enough”)
