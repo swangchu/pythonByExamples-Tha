0ed67c2ab25e71cf72d57a63d01805889125c9b9
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 15:37:51 +0600

    Create comparison.py

---
+++ example-2/comparison/comparison.py
@@ -0,0 +1,9 @@
+
+karma_height == dorji_height
+# Kama’s height is equal to dorji’s height
+
+ceo_sal != cto_sal
+# CEO’s salary is not equal to CTO
+
+engMark > dzoMark
+# English mark is greater than dzo
