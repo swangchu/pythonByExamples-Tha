9284746050b487cce868c2ee9ecc57b72dc1dbe7
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 16:08:35 +0600

    Create elif.py

---
+++ example-2/elif/elif.py
@@ -0,0 +1,15 @@
+
+age = int(input(“Your age:”))
+
+if age > 20:
+  
+   print(“Eligible to vote”)
+    
+# elif will have condition    
+elif age < 18:
+  
+   print(“Too young to vote”)
+    
+else:
+  
+   print(“Wait for sometime”)
