5a2991c8216dd4cc4aa0f0bdb605707b9624e3cd
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 19:20:25 +0600

    Create practice1.py

---
+++ example-3/practice-1-loop/practice1.py
@@ -0,0 +1,12 @@
+
+# Count is a variable that keeps count whenever the number is divisible by 5.
+
+count = 0
+
+for i in range(30):
+
+     if i % 5 == 0:
+     
+         count = count + 1
+         
+print(count)
