d3e3162bd6e563caa913793c99488960b2ec8577
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 15:46:01 +0600

    Create if.py

---
+++ example-2/if/if.py
@@ -0,0 +1,11 @@
+# This example demonstrate use of if statement.
+
+dzongkhaMark = 77
+
+# colon(:) is important
+if dzongkhaMark > 70:
+    
+     # Horizontal indentation is important for the delimitation
+     # some programming langugese uses braces({}) for delimitation
+    
+     print(“Passed with distinction”)
