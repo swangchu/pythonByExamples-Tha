28b5dc6c0ca70963736cc80e87bef6df1d3269c9
Sonam Wangchu <swangchu79@gmail.com>
2022-03-02 12:16:44 +0600

    Create input.py

---
+++ example-1/Input/input.py
@@ -0,0 +1,11 @@
+# name is variable 
+# input function gets the value from the user when the program is being run.
+# What's your name? will be displayed to the user
+# The user input will be stored in the variable name
+
+name = input(“What’s your name?”)
+
+# variable name is used in print function
+# name will be give what is stored in it; the value entered by the user.
+
+print(name)
