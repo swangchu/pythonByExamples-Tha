bd565e6b418454332e453d0ed522f94bbd047c38
Sonam Wangchu <swangchu79@gmail.com>
2022-03-03 15:07:03 +0600

    Create list.py

---
+++ example-5/List/list.py
@@ -0,0 +1,21 @@
+# This is an example of list and ways to access its elements 
+
+# subjects is a list containing three subjects
+
+subjects = ["dzo","eng","math"]
+
+print(subjects[0])
+#output : dzo
+
+print(subjects[1])
+#output : eng
+
+print(subjects[2])
+# output: math
+
+# The list can also be accessed by loops
+for sub in subjects:
+  
+     print(sub)
+    
+#Output: dzo, eng, math
