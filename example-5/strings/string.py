dd38aa2ebbc1797886fac35b6298c352b0c22477
Sonam Wangchu <swangchu79@gmail.com>
2022-03-03 14:55:31 +0600

    Create string.py

---
+++ example-5/strings/string.py
@@ -0,0 +1,19 @@
+# This example use string and its methods
+
+school = “Samtse HSS”
+# Samtse HSS is a string
+
+print(school.upper())
+# Output: SAMTSE HSS
+
+print(school.lower())
+# Output: samtse hss
+
+print(school.swapcase())
+#Output: sAMTSE hss
+
+print(school.find(“HSS”))
+# Output: 7, found at 7th index
+
+print(school.split())
+#Output: [‘Samtse’, ‘HSS’]
