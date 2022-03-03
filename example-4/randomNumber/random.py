fa17a825849faace7e0a98bb3ec313ec615b6315
Sonam Wangchu <swangchu79@gmail.com>
2022-03-03 11:35:29 +0600

    Create random.py

---
+++ example-4/randomNumber/random.py
@@ -0,0 +1,16 @@
+# This example uses the random module 
+
+# It import the random module 
+import random 
+
+# randint() generate a random number bewteen 1 and 10
+num = random.randint(1,10)
+
+print(num)
+
+# choice() method generate a random letter from ABCD
+letter = random.choice(“ABCDE”)
+
+print(letter)
+
+# This generates a letter out of the sequence ABCDE
