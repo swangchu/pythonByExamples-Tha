98a76edfd9218b124272d319a5e478f9d41f1700
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 09:26:09 +0600

    Create dict.py

---
+++ example-6/Dictionary/dict.py
@@ -0,0 +1,14 @@
+# This example uses dectionary another important data type in python
+
+# result is a dictionary
+result ={“eng”: 45, “math”:50}
+
+#access the value of key eng
+english_marks = result[“eng”]
+
+math_marks = result[“math”]
+
+# Dsiplay the values
+print(english_marks)
+
+print(math_marks)
