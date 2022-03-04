890beece1f149549ee6731924700a43f15064c1e
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 10:27:02 +0600

    Create para.py

---
+++ example-8/FunctionWithParameters/para.py
@@ -0,0 +1,15 @@
+# Example of functions with parameters
+
+#num1 and num2 are parameters
+
+def add(num1,num2):
+  
+     total = num1 + num2
+    
+     print(total)
+      
+#20 and 30 are arguments 
+
+add(20,30) # Output: 50 
+
+add(7,21) #Output:28
