7e99d896424af5dd24dec56cdc04e35a1bb2696a
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 10:58:20 +0600

    Create miss.py

---
+++ example-9/EmptySetMisunderstood/miss.py
@@ -0,0 +1,13 @@
+# Example clarify dictionary and set
+
+studnet_code ={}
+
+print(type(student_code))
+
+ # <class ‘dict’>
+  
+ student_code =set()
+
+ print(type(student_code))
+  
+ # <class ‘set’>
