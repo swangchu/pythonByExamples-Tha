d007563862adf4679d69859461866a68e0147dd1
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 09:20:29 +0600

    Create tuple method.py

---
+++ example-6/TupleMethod/tuple method.py
@@ -0,0 +1,14 @@
+# This example use Tuple methods
+
+marks = (45,59,45,77,35,45)
+
+# get count of 45 in the tuple
+# count() method counts number of 45 in the tuple
+
+count = marks.count(45)
+
+index = marks.index(77)
+
+print(“No. of 45 is ”,count)
+
+print("77 is at index ",index)
