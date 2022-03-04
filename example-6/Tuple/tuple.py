9bb095cadbf6f8a85485bdbfa509f79705084f17
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 09:15:58 +0600

    Create tuple.py

---
+++ example-6/Tuple/tuple.py
@@ -0,0 +1,17 @@
+# Example for the use of tuple
+# tuple is another important data structure in python
+
+# Declare a tuple
+bank_acc = ("BoB", 1232323)
+
+# Access element from the tuple
+bank = bank_acc[0]
+
+# Access the second element
+acc_no = bank_acc[1]
+
+# Displaying the elements
+
+print("Bank name:",bank)
+
+print("Acc No. ",acc_no)
