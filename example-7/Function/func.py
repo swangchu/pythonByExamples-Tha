27a61704ea7d86208f69e133d567a0c3ee665cf9
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 10:02:09 +0600

    Create func.py

---
+++ example-7/Function/func.py
@@ -0,0 +1,15 @@
+# Example uses function
+
+# This is how function is defined
+
+def greeting():
+  
+     print("Hello, World!")
+    
+# call function for 3 times
+
+greeting() # Hello, World!
+
+greeting()  # Hello, World!
+
+greeting() # Hello, World!
