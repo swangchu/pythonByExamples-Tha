a8ff39d1c111383794a9d934bf443ddfd269e14b
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 10:09:33 +0600

    Create return.py

---
+++ example-7/ReturnValueFunction/return.py
@@ -0,0 +1,14 @@
+# Example use function to return its value
+
+num1 = 10
+
+num2 = 15
+
+def sum_of_nums():
+  
+     sum = num1 + num2
+    
+     return sum
+    
+# Whoever call the function the sum of the number is returned
+print(sum_of_nums)
