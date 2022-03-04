54766537d5df8c856f577b5a2c6d534531dd89a6
Sonam Wangchu <swangchu79@gmail.com>
2022-03-04 10:35:22 +0600

    Create project9.py

---
+++ example-8/Project-9/project9.py
@@ -0,0 +1,25 @@
+# This project implements a Mad Lib
+
+def introduction():
+  
+        print("Enter the info")
+    
+        name =input("Name:")
+      
+        nat= input("nationality:")
+        
+        work =input("Work place:")
+        
+        ch =input("character")
+        
+        year = input("Year:")
+        
+        filmN = input("Film name:")
+        
+        grade = input("Grade:")
+        
+        school=input("School")
+        
+        print("Dasho "+name+" is a"+nat+" actor and researcher at the "+work+".He played the "+ch+" in the"+name+" completed class "+grade+" from"+school+".")
+
+ introduction()
